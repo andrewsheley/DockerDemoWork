600f3811cfd29b462b804657e523bc3bf1c53e81
Andrew Sheley KDOR <Andrew.Sheley@kdor.ks.gov>
2018-07-26 09:45:59 -0500

    Add project files.

---
+++ DockerDemo/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerDemo.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+       
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+
+
+
+
+}
